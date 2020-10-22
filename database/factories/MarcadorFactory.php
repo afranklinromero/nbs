c6d08344e841ad9f6f431efb976c274addb55a84
alfredomontoya <39633080+alfredomontoya@users.noreply.github.com>
2020-10-22 16:49:20 -0400

    221020201649

--- database/factories/MarcadorFactory.php
+++ database/factories/MarcadorFactory.php
@@ -13,11 +13,11 @@ $factory->define(Marcador::class, function (Faker $faker) {
     return [
         //
         'libro_id' => Libro::all('id')->random(),
-        'padre_id' => Marcador::all('id')->random(),
+        //'padre_id' => Marcador::all('id')->random(),
         'tipomarcador_id' => Tipomarcador::all('id')->random(),
         'numero' => $faker->numberBetween(1, 10),
         'nivel' => $faker->numberBetween(1, 4),
-        'espadre' => $faker->numberBetween(0, 1),
+        //'espadre' => $faker->numberBetween(0, 1),
         'nombre' => $faker->name,
         'pagina' => $faker->numberBetween(1, 100),
         'vistaprevia' => $faker->text(125),
