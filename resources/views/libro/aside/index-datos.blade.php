05b3ee3434602da0145ee104a75b788724fe5a4a
alfredomontoya <39633080+alfredomontoya@users.noreply.github.com>
2020-11-30 15:53:51 -0400

    30112020-1553 alfredo

--- resources/views/libro/aside/index-datos.blade.php
+++ resources/views/libro/aside/index-datos.blade.php
@@ -1,21 +1,32 @@
 <table class="table">
     <tbody>
+        @if (count($libros->items())>0)
         @foreach($libros as $libro)
-        <tr>
-            <td>
-                <img src="{{ asset('tapas/') }}/{{ $libro->tapa}}" class="rounded" width="100" alt="" srcset="">
-            </td>
-            <td>
-            <p class="text-dark"> <strong>libro ›  </strong> <span class="text-secondary"> {{$libro->documentopdf}}</span> &nbsp; <strong>id: </strong>{{$libro->id }}</p>
-                <h4 class="text-info"><a href="{{ route('libro.show', $libro->id) }}"> {{ $libro->titulo }}</a> </h4>
-                <p class="text-muted">
-                    <strong class="text-lowercase">serie › </strong> {{ $libro->serie}} <br>
-                    <strong class="text-lowercase">lugar publicacion › </strong> {{ $libro->lugarpublicacion}}<br>
-                    <strong class="text-lowercase">numero publicación › </strong> {{ $libro->nropublicacion}}<br>
-                </p>
-            </td>
-        </tr>
-        @endforeach
+            <tr>
+                <td>
+                    <img src="{{ asset('tapas/') }}/{{ $libro->tapa}}" class="rounded" width="100" alt="" srcset="">
+                </td>
+                <td>
+                <p class="text-dark"> <strong>libro ›  </strong> <span class="text-secondary"> {{$libro->documentopdf}}</span> &nbsp; <strong>id: </strong>{{$libro->id }}</p>
+                    <h4 class="text-info"><a href="{{ route('libro.show', $libro->id) }}"> {{ $libro->titulo }}</a> </h4>
+                    <p class="text-muted">
+                        <strong class="text-lowercase">serie › </strong> {{ $libro->serie}} <br>
+                        <strong class="text-lowercase">lugar publicacion › </strong> {{ $libro->lugarpublicacion}}<br>
+                        <strong class="text-lowercase">numero publicación › </strong> {{ $libro->nropublicacion}}<br>
+                    </p>
+                </td>
+            </tr>
+            @endforeach    
+        @else
+        @php
+            if (isset($request->nombre)) echo $request->nombre;
+        @endphp
+        @if (isset($request->nombre) && ($request->nombre != ""))
+            <p>No se encontraron resultados</p>
+        @endif
+            
+        @endif
+        
     </tbody>
 </table>
 
