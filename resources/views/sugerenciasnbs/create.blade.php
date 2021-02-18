3339246ee0995c88ce0d37a3e9ed1473d92ff067
alfredomontoya <39633080+alfredomontoya@users.noreply.github.com>
2021-02-18 17:12:58 -0400

    sugerencias 2

--- resources/views/sugerenciasnbs/create.blade.php
+++ resources/views/sugerenciasnbs/create.blade.php
@@ -5,15 +5,34 @@
     <h3>ENVIAR COMENTARIO</h3>
     {!! Form::open(['route'=>'sugerenciasnbs.store']) !!}
         {{ csrf_field() }}
-        {!! Form::label('name', 'Nombre:', ['class' => 'form-group']) !!}
-        {!! Form::text('name', 'Alfredo', ['class' => 'form-group']) !!}
-        {!! Form::label('email', 'Correo:', ['class' => 'form-group']) !!}
-        {!! Form::text('email', 'micorreo@nbs.com', ['class' => 'form-group']) !!}
-        {!! Form::label('subject', 'Asunto:', ['class' => 'form-group']) !!}
-        {!! Form::text('subject', 'Este es mi asunto', ['class' => 'form-group']) !!}
-        {!! Form::label('content', 'Contenido:', ['class' => 'form-group']) !!}
-        {!! Form::text('content', 'este es mi contenido', ['class' => 'form-group']) !!}
+        <div class="mb-3">
+        {!! Form::label('name', 'Nombre:', ['class' => 'form-label']) !!}
+        {!! Form::text('name', 'Alfredo', ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="mb-3">
+        {!! Form::label('email', 'Correo:', ['class' => 'form-label']) !!}
+        {!! Form::text('email', 'micorreo@nbs.com', ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="mb-3">
+        {!! Form::label('subject', 'Asunto:', ['class' => 'form-label']) !!}
+        {!! Form::text('subject', 'Este es mi asunto', ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="mb-3">
+        {!! Form::label('content', 'Contenido:', ['class' => 'form-label']) !!}
+        {!! Form::text('content', 'este es mi contenido', ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="mb-3">
         {!! Form::submit('Enviar', ['class' => 'btn btn-primary']) !!}
+        </div>
+        
+        
+        
+        
+        
     {!! Form::close() !!}
 
 
