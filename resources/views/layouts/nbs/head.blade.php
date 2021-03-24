e3b09902023e6173422f210629f0e565c2b146c7
choco <afranklin.romero@gmail.com>
2021-03-24 10:34:09 -0400

    analitica

--- resources/views/layouts/nbs/head.blade.php
+++ resources/views/layouts/nbs/head.blade.php
@@ -21,4 +21,17 @@
     <script src="{{ asset('dropzone.js')}}"></script>
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
     <!--<script src="{{asset('js/app.js')}}"></script>-->
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LJWQ3NJ1EG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LJWQ3NJ1EG');
+</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+
 </head>
