41b9c89abdb2a4c84a1cd1d1169a443b9d9d7782
alfredomontoya <39633080+alfredomontoya@users.noreply.github.com>
2021-04-05 21:42:58 -0400

    aun no funciona

--- resources/views/libro/aside/show-right.blade.php
+++ resources/views/libro/aside/show-right.blade.php
@@ -1,6 +1,6 @@
 <!--<iframe src="https://docs.google.com/gview?embedded=true&url={{asset('libros') }}/{{$documentopdf}}#page={{$pagina}}" style="width:100%;" height="100%" frameborder="0"></iframe>-->
 <!--<iframe src="https://docs.google.com/gview?embedded=true&url=https://normasbolivianasdesalud.com/libros/37-malaria.pdf#page=10" style="width:100%;" height="100%" frameborder="0"></iframe>-->
 <!--<iframe id="iframe1" src="http://docs.google.com/gview?url=normasbolivianasdesalud/libros/{{ $documentopdf }}&embedded=true#:0.page.{{ $pagina }}">-->
-    <iframe src="http://docs.google.com/gview?url=https://normasbolivianasdesalud.com/libros/{{ $documentopdf }}&embedded=true&a=bi&pagenumber={{ $pagina }}" style="width:100%;" height="100%">
+    <iframe src="http://docs.google.com/gview?embedded=true&url=https://normasbolivianasdesalud.com/libros/{{ $documentopdf }}#:0.page.{{ $pagina }}" style="width:100%;" height="100%">
 
 <!--<embed src="{{asset('libros')}}/{{$documentopdf}}#page={{$pagina}}" type='application/pdf' width='100%' height='100%'/>-->
