92424594a274f846b18a52c2f95f05d0b603651b
alfredomontoya <39633080+alfredomontoya@users.noreply.github.com>
2021-05-04 16:03:58 -0400

    blog correccion...

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Modelos\Blog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BlogController extends Controller
 {
@@ -20,5 +21,19 @@ class BlogController extends Controller
 
     public function create(Request $request){
         return view('blog.create');
+    }
+
+    public function store(Request $request){
+        $blog = new Blog($request->all());
+
+        $blog->estado = 1;
+
+        $blog->save();
+        //dd($request->multimedia);
+        $request->file('multimedia')->store('public/img/blog/');
+
+        if ($request->ajax())
+            return redirect()->route('blog.show', $blog->id);
+        return redirect()->route('blog.show', $blog->id);
     }
 }
