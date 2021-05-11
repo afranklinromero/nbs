4ff41e3568c66e4886c5db4b57ffedf941d59f3e
alfredomontoya <39633080+alfredomontoya@users.noreply.github.com>
2021-05-10 21:57:41 -0400

    Update ConcursoSeeder.php

--- database/seeds/ConcursoSeeder.php
+++ database/seeds/ConcursoSeeder.php
@@ -18,7 +18,7 @@ class ConcursoSeeder extends Seeder
         //Concurso::create(['nombre'=>'1er Concurso Malaria', 'picture' => 'concursomalaria.png', 'configuracion_id' => 1, 'fechaini' => '2021-01-01', 'fechafin' => '2021-01-30']);
         Concurso::create(['nombre'=>'1er Concurso Tuberculosis', 'picture' => 'tuberculosis.jpg', 'configuracion_id' => 1, 'fechaini' => '2021-01-01', 'fechafin' => '2021-12-30']);
 
-        Temaconcurso::create(['tema_id'=> 1, 'concurso_id'=>1,]);
+        Temaconcurso::create(['tema_id'=> 2, 'concurso_id'=>1,]);
         //Temaconcurso::create(['tema_id'=> 2, 'concurso_id'=>2,]);
 
 
